d003a86373879cd16fdeb008596aad4a5534bbce
Carlos Cabañero <carlos.cabanero@Carloss-MacBook-Pro.local>
2022-02-08 11:01:50 +0100

    Updated dotfiles

--- bin/changemac.sh
+++
@@ -1,4 +0,0 @@
-mac=$(openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//')
-sudo /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -z
-sudo ifconfig en0 ether $mac
-networksetup -detectnewhardware
